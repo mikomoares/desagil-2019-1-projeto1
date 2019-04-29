65357301e04068241f4f584fbe2b31e6b2137827
roguetaver <andretavernaro@gmail.com>
2019-04-29 16:43:01 -0300

    tela mensagem rapida

---
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/mensagens_rapidas.java
@@ -0,0 +1,13 @@
+package br.pro.hashi.ensino.desagil.projeto1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mensagens_rapidas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mensagens_rapidas);
+    }
+}
