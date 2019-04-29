c24f4106c26a19f47e63e1c7abc0a087d9e7b172
Andre-Weber <andreew@al.insper.edu.br>
2019-04-29 13:40:15 -0300

    layout att, fix errors

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MorseTranslate.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MorseTranslate.java
@@ -1,23 +1,24 @@
-package br.edu.insper.al.andreew.testeproject;
+package br.pro.hashi.ensino.desagil.projeto1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import br.pro.hashi.ensino.desagil.projeto1.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MorseTranslate extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_morse_translate);
 
 
-        final Button ButtonSignal = findViewById(R.id.button_signal);
-        final TextView textview = findViewById(R.id.TextView);
+        final ImageButton ButtonSignal = findViewById(R.id.button_signal);
+        final TextView textview = findViewById(R.id.morse_text);
         ButtonSignal.setOnClickListener(new View.OnClickListener() {
 
             @Override
