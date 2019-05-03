33633099c614e4c22e4e68aed2701e3b1ef053c7
mikomoares <mihanoch@gmail.com>
2019-05-03 14:57:00 -0300

    correção Translate. Modificação MorseTranslate

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MorseTranslate.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MorseTranslate.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.util.LinkedList;
+
 public class MorseTranslate extends AppCompatActivity {
 
     @Override
@@ -14,22 +16,49 @@ public class MorseTranslate extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_morse_translate);
 
-        final ImageButton ButtonSignal = findViewById(R.id.button_signal);
+        final Translator translator = new Translator();
+        final ImageButton buttonBack = findViewById(R.id.backspace);
+        final ImageButton buttonSignal = findViewById(R.id.button_signal);
         final TextView textmorse = findViewById(R.id.morse_text);
-        final Button ButtonSpace = findViewById(R.id.whitespace);
+        final Button buttonSpace = findViewById(R.id.whitespace);
         final TextView texttrad = findViewById(R.id.translated_text);
+        LinkedList<String> lista = translator.getCodes();
+        System.out.println(lista);
 
-        ButtonSignal.setOnLongClickListener((view) -> {
-            textmorse.setText(textmorse.getText() + "-");
+        buttonSignal.setOnLongClickListener((view) -> {
+            if (textmorse.getText().length()<5) {
+                textmorse.setText(textmorse.getText() + "-");
+            }
             return true;
         });
 
-        ButtonSignal.setOnClickListener((view) -> textmorse.setText(textmorse.getText() + "."));
-
-        ButtonSpace.setOnClickListener((view) -> {
-            textmorse.setText(textmorse.getText() + "  ");
-
+        buttonSignal.setOnClickListener((view) -> {
+            if (textmorse.getText().length()<5) {
+                textmorse.setText(textmorse.getText() + ".");
+            }
         });
 
+        buttonSpace.setOnClickListener((view) -> {
+            //if (lista.contains(textmorse.toString())) {
+                texttrad.setText(texttrad.getText().toString() + translator.morseToChar(textmorse.getText().toString()));
+                textmorse.setText("");
+           // }
+        });
+
+        buttonBack.setOnClickListener((view) -> {
+            String morse = textmorse.getText().toString();
+            String alpha = texttrad.getText().toString();
+            int tamanhom = morse.length();
+            int tamanhoa = alpha.length();
+            if (tamanhom != 0) {
+                String novo = morse.substring(0, tamanhom - 1);
+                textmorse.setText(novo);
+            } else if (tamanhoa != 0){
+                String novo = alpha.substring(0, tamanhoa - 1);
+                texttrad.setText(novo);
+            } else{
+                texttrad.setText("");
+            }
+        });
     }
 }
